e4cfb50f95b674301367474cde3282bbfd5ac488
Martin Karg <mkarg94@gmail.com>
2017-03-28 22:56:01 +0000

    Add all

---
+++ gui-python/sliders.py
@@ -0,0 +1,68 @@
+import kivy
+kivy.require('1.9.0')
+ 
+from kivy.app import App
+from kivy.lang import Builder 
+from kivy.uix.slider import Slider
+from kivy.uix.button import Label
+ 
+# Uncomment for seeing it outside of RPi
+# import RPi.GPIO as GPIO
+# GPIO.setmode(GPIO.BOARD)
+ 
+# RED , GREEN, BLUE = 11, 12, 13 # BOARD numbering
+# GPIO.setup([RED, GREEN, BLUE], GPIO.OUT)
+ 
+# # setup sofware pulse width modulation - pwm
+# red_pwm = GPIO.PWM(RED, 100)
+# green_pwm = GPIO.PWM(GREEN, 100)
+# blue_pwm = GPIO.PWM(BLUE, 100)
+ 
+# # start duty cycle with the same value as the sliders
+# red_pwm.start(50)
+# green_pwm.start(50)
+# blue_pwm.start(50)
+ 
+# Create .kv string
+kv = '''
+BoxLayout:
+    orientation: 'vertical'
+    MySlider:
+        mycolor: 'red'
+        on_value: self.set_color(*args)
+    MySlider:
+        mycolor: 'green'
+        on_value: self.set_color(*args)
+    MySlider:
+        mycolor: 'blue'
+        on_value: self.set_color(*args)        
+'''
+ 
+class MySlider(Slider):
+    def __init__(self, **kwargs):
+        kwargs['min'] = 0
+        kwargs['max'] = 100
+        kwargs['value'] = 50
+        super (MySlider, self).__init__(**kwargs)    
+ 
+    def set_color(self, instance, value):
+        if self.mycolor == 'red':
+            #red_pwm.ChangeDutyCycle(int(value))
+            test = 'red'
+        elif self.mycolor == 'green':
+            #green_pwm.ChangeDutyCycle(int(value))
+            test = 'green'
+        elif self.mycolor == 'blue':
+            #blue_pwm.ChangeDutyCycle(int(value))
+            test = 'blue'
+ 
+class AmbientApp(App):
+    def build(self):
+        return Builder.load_string(kv)
+ 
+if __name__ == '__main__':
+    try:
+        AmbientApp().run() 
+    except KeyboardInterrupt:
+        #GPIO.cleanup()
+        test = 'exit'
