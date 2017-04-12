ee9578fad2543d6bff269d63846eea4301bfdc92
Martin Karg <mkarg94@gmail.com>
2017-04-12 00:21:32 +0000

    Reestructured folder structure

--- Robot/gui-python/kivy-tests-mk/useful-snippets/config.py
+++
@@ -1,7 +0,0 @@
-from kivy.config import Config 
-
-# Configuration of kivy https://kivy.org/docs/api-kivy.config.html
-# Config.set('graphics', 'fullscreen', 'auto')
-# Config.set('graphics', 'borderless', '1')
-Config.set('kivy', 'exit_on_escape', '1')
-Config.set('kivy', 'keyboard_mode', 'systemanddock')
--- Robot/gui-python/kivy-tests-mk/useful-snippets/sound.py
+++
@@ -1,7 +0,0 @@
-from kivy.core.audio import SoundLoader
-
-sound = SoundLoader.load('mywav.wav')
-if sound:
-    print("Sound found at %s" % sound.source)
-    print("Sound is %.3f seconds" % sound.length)
-    sound.play()
