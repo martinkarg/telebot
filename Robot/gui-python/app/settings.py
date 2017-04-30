273307e9bae51b57361e58864672895f9ea7d31f
Martin Karg <Martin Karg>
2017-04-30 23:55:14 +0200

    updated url

--- Robot/gui-python/app/settings.py
+++ Robot/gui-python/app/settings.py
@@ -117,7 +117,7 @@ Robot_ID = Robot_Password = "robot01"
 
 def PlaceCall():
     robot_login = "https://connection-robertoruano.c9users.io/robot_login.php?username=" + Robot_ID + "&pswrd=" + Robot_Password
-    os.system("sudo su -c 'chromium-browser' -s /bin/sh pi")
+    os.system("sudo su -c \"chromium-browser '" + robot_login + "'\" -s /bin/sh pi")
     return robot_login
 
 def ErrorLog(message):
